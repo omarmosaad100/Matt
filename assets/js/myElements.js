83123fbe5126fce6b492970998d9c23878ec563d
Sherif Gabr <50206867+shgabr@users.noreply.github.com>
2022-12-23 15:51:21 +0200

    final

--- assets/js/myElements.js
+++ assets/js/myElements.js
@@ -26,8 +26,8 @@ class MyHeader extends HTMLElement {
                                 <div class="header-info-mid">
                                     <!-- logo -->
                                     <div class="logo">
-                                        <!-- <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a> -->
-                                        <a href="index.html"><h1>Matt</h1></a>
+                                        <a href="index.html"><img src="assets/img/logo/logo.png" alt="" style="width: 200px;"></a> 
+                                        <!-- <a href="index.html"><h1>Matt</h1></a> -->
                                     </div>
                                 </div>
                                 <div class="header-info-right d-flex align-items-center">
@@ -36,12 +36,12 @@ class MyHeader extends HTMLElement {
                                     <li><a href="contact.html">Contact</a></li>
                                     <li><a href="login.html">Log In  or  Sign Up</a></li> -->
                                     <li><a href="about.html">About</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
+                                    <!-- <li><a href="contact.html">Contact</a></li> -->
                                 </ul>
                                 <!-- Social -->
                                 <div class="header-social">
-                                        <a href="#"><i class="fab fa-twitter"></i></a>
-                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="https://twitter.com/mattmagazineEG"><i class="fab fa-twitter"></i></a>
+                                        <a href="https://fb.me/mattmagazine"><i class="fab fa-facebook-f"></i></a>
                                         <a href="https://instagram.com/matt_magazine"><i class="fab fa-instagram"></i></a>
                                     </div>
                                 </div>
@@ -56,15 +56,15 @@ class MyHeader extends HTMLElement {
                                 <!-- logo 2 -->
                                 <div class="logo2">
                                     <a href="index.html">
-                                        <!-- <img src="assets/img/logo/logo.png" alt=""> -->
-                                        <h1>Matt</h1>
+                                        <img src="assets/img/logo/logo.png" alt="" style="width: 200px;">
+                                        <!-- <h1>Matt</h1> -->
                                     </a>
                                 </div>
                                 <!-- logo 3 -->
                                 <div class="logo3 d-block d-sm-none">
                                     <a href="index.html">
-                                        <!-- <img src="assets/img/logo/logo-mobile.png" alt=""> -->
-                                        <h1>Matt</h1>
+                                        <img src="assets/img/logo/logo-mobile.png" alt="" style="width: 100px;"> 
+                                        <!-- <h1>Matt</h1> -->
                                     </a>
                                 </div>
                                 <!-- Main-menu -->
@@ -72,9 +72,9 @@ class MyHeader extends HTMLElement {
                                     <nav>                                                
                                         <ul id="navigation">                                                                                                                                     
                                             <li><a href="art.html">Art</a></li>
-                                            <li><a href="category.html">Fashion</a></li>
-                                            <li><a href="category.html">Lifestyle</a></li>
-                                            <li><a href="category.html">Places</a></li>
+                                            <li><a href="fashion.html">Fashion</a></li>
+                                            <li><a href="lifestyle.html">Lifestyle</a></li>
+                                            <li><a href="places.html">Places</a></li>
                                         </ul>
                                     </nav>
                                 </div>
@@ -105,21 +105,21 @@ class MyFooter extends HTMLElement {
                                 <div class="header-info-left d-flex">
                                     <!-- Social -->
                                     <div class="header-social">
-                                        <a href="#"><i class="fab fa-twitter"></i></a>
-                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                        <a href="https://twitter.com/mattmagazineEG"><i class="fab fa-twitter"></i></a>
+                                        <a href="https://fb.me/mattmagazine"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="https://instagram.com/matt_magazine"><i class="fab fa-instagram"></i></a>
                                     </div>
                                 </div>
                                 <div class="header-info-mid">
                                     <!-- logo -->
                                     <div class="logo">
-                                        <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                                        <a href="index.html"><img src="assets/img/logo/logo.png" alt="" style="width: 200px;"></a>
                                     </div>
                                 </div>
                                 <div class="header-info-right d-flex align-items-center">
                                    <ul>                                          
                                        <li><a href="about.html">About</a></li>
-                                       <li><a href="contact.html">Contact</a></li>
+                                       <!-- <li><a href="contact.html">Contact</a></li> -->
                                    </ul>
                                 </div>
                             </div>
@@ -149,5 +149,231 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
     }
 }
 
+class myArtLatest extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+        <div class="latest-posts pt-80">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section-tittle mb-35">
+                            <h2>Latest Posts</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/S5 Puppet show/img3 cropped.JPG" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                    <h3><a href="_S5_puppet_show.html">A show that has been going for five thousand years</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/H2 Fo5ar/img2.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_H2_Fokhar.html">Fokhar.. The Art of patience</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/S2 Museum/img3 cropped.JPG" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_S2_Museum.html">A Museum of Art, but this time, feeling a bit different</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+
+class myFashionLatest extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+        <div class="latest-posts pt-80">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section-tittle mb-35">
+                            <h2>Latest Posts</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/HM4 Made In Egypt/img2.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                    <h3><a href="_HM4_Made_in_Egypt.html">Made in Egypt</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/HM6 Wekala/wekala2.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_HM6_Wekala.html">Yes, mom, I thrifted AGAIN at the Wekala</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/HM1  Extinction of Galabeya/img3 cropped.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_HM1_The_extinction_of_galabeya.html">How it started, and how it's ending.. The Extinction of Galabeya</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+class myLifestyleLatest extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+        <div class="latest-posts pt-80">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section-tittle mb-35">
+                            <h2>Latest Posts</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/M6 Zoo/zoo2.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                    <h3><a href="_M6_Zoo.html">Zoo: Putting animals in cages for visitors to enjoy their day</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/O6 kids life style/img3.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_O6_kids_lifestyle.html">Will technology create a lazy generation?</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/R1 Therapy/img2.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_R1_Therapy.html">Therapy: A Way to Improve Your General Health</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+class myPlacesLatest extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+        <div class="latest-posts pt-80">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section-tittle mb-35">
+                            <h2>Latest Posts</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/O8 Circus/img4.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                    <h3><a href="_O8_Circus.html">A show that only kids love: The National Circus of Agouza</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/H1 Darb 18/img3 cropped.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_H1_Darb_18.html">The cultural hub of Darb 17 18</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-baner-nw2 mb-30">
+                            <div class="banner-img-cap2">
+                                <div class="banner-img">
+                                    <img src="assets/img/articles/O1 Gezira/img4.jpeg" alt="">
+                                </div>
+                                <div class="banner-cap2">
+                                   <h3><a href="_O1_Gezira.html">Geziret El Dahab: A couple of kilometers away from heaven</a></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+
 customElements.define("my-header", MyHeader);
 customElements.define("my-footer", MyFooter);
+customElements.define("art-latest", myArtLatest);
+customElements.define("fashion-latest", myFashionLatest);
+customElements.define("lifestyle-latest", myLifestyleLatest);
+customElements.define("places-latest", myPlacesLatest);
---
+++ js/searchSugg.js
@@ -0,0 +1,146 @@
+let suggestions = [
+    "Reveling in Silence",
+    "Wooden Art craft",
+    "Representation Through the Cinematic Lense",
+    "Bedouin Market",
+    "I am the Clown! The story of Clown Kajo",
+    "A Vintage Taste for Music: Sherry's Vinyl",
+    "Arabic Calligraphy",
+    "Caricature, the lost laughing art",
+    "A show that has been going for five thousand years",
+    "Fokhar.. The Art of patience",
+    "A Museum of Art, but this time, feeling a bit different",
+
+    "Nubian Fashion",
+    "Bedouin Fashion",
+    "Upper Egyptian Fashion",
+    "Coastal fashion",
+    "Farmers in style",
+    "Yes, mom, I thrifted AGAIN at the Wekala",
+    "Made in Egypt",
+    "How it started, and how it's ending.. The Extinction of Galabeya",
+
+    "The Ingenious Nine-Year-Old Girl",
+    "Living on the Nile	",
+    "Transportation through river in Egypt.. like blood run in veins",
+    "Rabbit: Electric scooters for rent",
+    "Feel free to be different in.. Dahab",
+    "Veganism: Do we need to eat animals? Or do they just taste good?",
+    "Sun, the battery that never dies",
+    "Are robots taking over?",
+    "The nile is not 100% water!",
+    "Zoo: Putting animals in cages for visitors to enjoy their day",
+    "Will technology create a lazy generation?",
+    "Therapy: A Way to Improve Your General Health",
+
+    "Weesa Wassef, a magical piece of art on the edges of Giza",
+    "An Artist's Safe Zone: The Comfort Zone",
+    "Dahab.. All the diversity you can get",
+    "Egypt is exploring the space!",
+    "Welcome to a Park Where the Pets Roam Free: PetsVille",
+    "Souq El Gomaa \"We Sell Every Old Thing\"",
+    "A show that only kids love: The National Circus of Agouza",
+    "The cultural hub of Darb 17 18",
+    "Geziret El Dahab: A couple of kilometers away from heaven",
+]
+
+const page = new Map([
+    ["Reveling in Silence","_HM2_Art_gallery.html"],
+    ["Wooden Art craft","_S3_wooden_craft.html"],
+    ["Representation Through the Cinematic Lense",""],
+    ["Bedouin Market","_HM3_Scenes.html"],
+    ["I am the Clown! The story of Clown Kajo","_M2_Clown.html"],
+    ["A Vintage Taste for Music: Sherry's Vinyl","_R2_sherry_s.html"],
+    ["Arabic Calligraphy", "_S4_calligraphy.html"],
+    ["Caricature, the lost laughing art","_S1_Caricature.html"],
+    ["A show that has been going for five thousand years","_S5_puppet_show.html"],
+    ["Fokhar.. The Art of patience","_H2_Fokhar.html"],
+    ["A Museum of Art, but this time, feeling a bit different","_S2_Museum.html"],
+
+    ["Nubian Fashion","_A1_nouba.html"],
+    ["Bedouin Fashion","_A2_Beddouin.html"],
+    ["Upper Egyptian Fashion","_A4_upper_egyptians.html"],
+    ["Coastal fashion","_A5_Coastal_fashion.html"],
+    ["Farmers in style","_A3_fallaheen.html"],
+    ["Yes, mom, I thrifted AGAIN at the Wekala","_HM6_Wekala.html"],
+    ["Made in Egypt","_HM4_Made_in_Egypt.html"],
+    ["How it started, and how it's ending.. The Extinction of Galabeya","_HM1_The_extinction_of_galabeya.html"],
+
+    ["The Ingenious Nine-Year-Old Girl","_M1_girl.html"],
+    ["Living on the Nile","_M3_Sayadeen.html"],
+    ["Transportation through river in Egypt.. like blood run in veins","_O2_Na2l_Nahry.html"],
+    ["Rabbit: Electric scooters for rent","_O3_Rabbit.html"],
+    ["Feel free to be different in.. Dahab","_O5_Dahab_Lifestyle.html"],
+    ["Veganism: Do we need to eat animals? Or do they just taste good?","_O7_Veganism.html"],
+    ["Sun, the battery that never dies","_O9_solar_energy.html"],
+    ["Are robots taking over?","_O11_Robots.html"],
+    ["The nile is not 100% water!","_O12_Nile_Pollution.html"],
+    ["Zoo: Putting animals in cages for visitors to enjoy their day","_M6_Zoo.html"],
+    ["Will technology create a lazy generation?","_O6_kids_lifestyle.html"],
+    ["Therapy: A Way to Improve Your General Health","_R1_Therapy.html"],
+
+    ["Weesa Wassef, a magical piece of art on the edges of Giza","_M4_5eyata.html"],
+    ["An Artist's Safe Zone: The Comfort Zone","_R3_comfortzone.html"],
+    ["Dahab.. All the diversity you can get","_O4_Dahab_Place.html"],
+    ["Egypt is exploring the space!","_O10_Space_stations.html"],
+    ["Welcome to a Park Where the Pets Roam Free: PetsVille","_R4_petsville.html"],
+    ["Souq El Gomaa \"We Sell Every Old Thing\"","_M5_Souq_el_gomaa.html"],
+    ["A show that only kids love: The National Circus of Agouza","_O8_Circus.html"],
+    ["The cultural hub of Darb 17 18","_H1_Darb_18.html"],
+    ["Geziret El Dahab: A couple of kilometers away from heaven","_O1_Gezira.html"]
+  ]);
+
+//getting required elements
+const searchWrapper = document.querySelector(".form-box");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+
+//if user enters value
+inputBox.onkeyup = (e) => {
+    // console.log(e.target.value);
+    let userData = e.target.value;
+    let emptyArray = [];
+    if (userData){
+        emptyArray = suggestions.filter((data) => {
+            if (data.toLocaleLowerCase().includes(userData)){
+                return data;
+            }
+        });
+        emptyArray = emptyArray.map((data) => {
+            return '<li>'+data+'</li>';
+        });
+
+        // console.log(emptyArray);
+        searchWrapper.classList.add("active");
+        showSuggestions (emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            allList[i].setAttribute("onclick", "select(this)");
+            
+        }
+
+    } else {
+        searchWrapper.classList.remove("active");
+    }
+}
+
+function select(element){
+    let selectUserData = element.textContent;
+    console.log(page.get(selectUserData));
+    // window.location.pathname = './post_details.html'
+    document.location.href =  page.get(selectUserData);
+}
+
+
+//show suggestions
+function showSuggestions (list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li></li>';
+        searchWrapper.classList.remove("active");
+    } else {
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
